37155e6fdc6d4731f22c98e226cd1330ca6560f8
luohino <aniketsingh821305@gmail.com>
2026-04-25 21:41:58 +0530

    mistakes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
 import { HelmetProvider } from 'react-helmet-async';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
